0d023560898291126f09c6b07da50ba25c9e2700
alishbahafeez241 <alishbahafeez241@gmail.com>
2026-06-05 09:25:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -336,19 +336,15 @@ def simulated_annealing(stocks, iterations=100, T_start=1000, T_end=0.01):
 # ─────────────────────────────────────────────
 
 with st.sidebar:
-
     st.markdown("""
-    
-    
-    <div style="
-   
+<div style="
     background: linear-gradient(135deg,#1d4ed8,#3b82f6);
     color:white;
     padding:18px;
     border-radius:14px;
     text-align:center;
     margin-bottom:20px;
-    ">
+">
     <div style="font-size:2.2rem;">📊</div>
 
     <div style="
@@ -360,6 +356,8 @@ with st.sidebar:
     </div>
 </div>
 """, unsafe_allow_html=True)
+    
+
     investor_name = st.text_input(
         "Enter Your Name",
         value="",
